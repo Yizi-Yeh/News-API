c8e302ce293afe9eda2368bc5e608f4a39c44c91
Yizi-Yeh <g384357@gmail.com>
2021-07-06 12:05:29 +0800

    add: get api

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,16 @@
 <template>
-  <el-container>
-    <el-header>News-API</el-header>
-    <el-main type="flex" justify="center">
-      <el-row>
-        <el-col
-          :xl="{ span: 12, offset: 6 }"
-          :lg="{ span: 12, offset: 6 }"
-          :md="{ span: 16, offset: 4 }"
-          :sm="{ span: 20, offset: 2 }"
-          :xs="24"
-          type="flex"
-          justify="center"
-        >
-          <el-card :body-style="{ padding: '0px' }">
-            <img
-              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-              class="image"
-            />
-            <div style="padding: 14px">
-              <span>好吃的汉堡</span>
-              <div class="bottom">
-                <time class="time">{{ currentDate }}</time>
-                <el-button type="text" class="button">操作按钮</el-button>
-              </div>
-            </div>
-          </el-card>
-        </el-col>
-      </el-row>
-    </el-main>
-    <el-footer><h4>Copyright 2021 all rights reserved</h4></el-footer>
-  </el-container>
+  <News/>
 </template>
+
+<script>
+import News from './views/News/index.vue'
+
+export default {
+  components: {
+    News
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
--- src/views/News/index.vue
+++ src/views/News/index.vue
@@ -1,5 +1,54 @@
 <template>
-  <div class="home"></div>
+  <el-container>
+    <el-header>News-API</el-header>
+    <el-main type="flex" justify="center">
+      <el-row>
+        <el-col
+          :xl="{ span: 12, offset: 6 }"
+          :lg="{ span: 12, offset: 6 }"
+          :md="{ span: 16, offset: 4 }"
+          :sm="{ span: 20, offset: 2 }"
+          :xs="24"
+          type="flex"
+          justify="center"
+        >
+          <div class="search-box">
+            <el-input
+              type="text"
+              class="search-bar"
+              placeholder="請輸入關鍵字"
+              v-model="query"
+              @keypress="fetchNews"
+            ></el-input>
+          </div>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col
+          :xl="{ span: 12, offset: 6 }"
+          :lg="{ span: 12, offset: 6 }"
+          :md="{ span: 16, offset: 4 }"
+          :sm="{ span: 20, offset: 2 }"
+          :xs="24"
+          v-for="(item, idx) in news.data"
+          :key="item"
+          :offset="idx > 0 ? 2 : 0"
+        >
+          <el-card>
+            <img :src="item.urlToImage" class="image" />
+            <div style="padding: 14px">
+              <span>好吃的汉堡</span>
+              <div class="bottom">
+                <el-button type="text" class="button">操作按钮</el-button>
+              </div>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </el-main>
+    <el-footer><h4>Copyright 2021 all rights reserved</h4></el-footer>
+  </el-container>
 </template>
 
 <script>
@@ -14,7 +63,7 @@ export default {
     const url = ref('https://newsapi.org/v2/')
     const query = ref('COVID-19')
     const sort = ref('popularity')
-    let news = reactive({})
+    const news = reactive({ data: {} })
 
     onMounted(() => {
       fetchNews()
@@ -26,7 +75,8 @@ export default {
           `${url.value}everything?q=${query.value}&from=2021-07-04&to=2021-07-04&sortBy=${sort.value}&apiKey=${apikey.value}`
         )
         .then((res) => {
-          news = res.data
+          news.data = res.data.articles
+          console.log(news)
         })
     }
 
